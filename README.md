ab2a40d1650363a4ed077de768f4aa3b5a51fd86
Gleide Coelho <gleidecn3@gmail.com>
2021-10-07 08:20:33 +0000

    I´ve done the accessibility and performance teste. Also I´ve used the html, css and javascript validator

--- README.md
+++ README.md
@@ -1,3 +1,103 @@
 # Quiz Time
 
+The Quiz game is a fun app with four questions. it has as well the score. So when the user is answering he can see if he got the right or wrong questions. 
+
+[Live link](https://example.com)
+
+My onw pic
+
+![Image text](../images/website-min.png)
+
+### 
+
+# Table of Contents
+
+1. [UX](#general-info)
+
+    i. First Time Visitor Goals:
+    - As a first time visitor I need to be able to navigate the site easily and clearly understand how to use the function of the site.
+    - The users will easly choose the options and go to the next question.
+
+    ii.  Returning Visitor Goals
+    - They can recomemnd the site and see from his group who got most points.
+    
+3. [Design](#design)
+
+    -  Colour Scheme:
+![alt tag](./images/colours-min.png "Colour scheme")
+
+- Typography:
+    - Site wide, Archivo is used. Being provided by google fonts.
+    - This font was chosen as it looks modern and is easy to read.
+    - Wireframes
+
+    - Layout
+
+4. [Features](#features)
+- Existing Features
+- Future Features
+    - I want to add images to be more interactive with the users. Depending on their answer it will be a diferent image.
+
+5. [Technologies Used](#technologies Used)
+1. HTML: 
+    - HTML was used for the content and structure of the site, with emphasis placed on semantic elements and text alternatives for screen readers.
+2. was used to assist with the responsiveness and styling of the website.
+3. [Google Font](https://fonts.google.com)
+    - I´ve import the 'Monserrat' font into the style.css file which is used throughout the project.
+    4. [Git](https://git-scm.com)
+    - Git was used for version control by utilizing the Gitpod terminal to commit to Git and Push to GitHub.
+    5. [Github](https://github.com/gleidecn/quiz-time)
+    - GitHub is used to store the project's code after being pushed from Git.
+    6. [Gitpod](https://gitpod.io/workspaces)
+    - This was my IDE for the project.
+
+6. [Testing](#testing)
+- Testing was carried out both manually and automatically.
+
+- Manual Testing
+    - While building the site I had a preview open in my browser that I would check regularly to ensure that the results were as intended. I made use of developer tools within the browser constantly to check across a number of things. I would use the console tools to try out different ideas, particularly styling, to see how it worked in real time and then implement the code written there into the project itself. Responsiveness testing was done both through console tools, along with trying different browsers on different devices.
+
+- Automatic Testing
+    - Performance and accessiblilty:
+[Google Lighthouse](https://developers.google.com/web/tools/lighthouse/)    
+![alt tag](./images/passing-the-metrics-min.png "Validation")
+
+    - Bugs
+    - Known Issues
+6. [Deployment](#deployment)
+- I deployed this site through GitHub pages. This was a simple process, as follows:
+    - Log into GitHub
+    - Go to the repository (you must own it, so fork mine if deploying this site)
+    - Go to settings
+    - Scroll down to "GitHub Pages" section
+    - Select the source (I chose master branch in this instance)
+    - After some time, the site will be deployed at: http://<username>.github.io/<repository-name>
+
+- Making A Local Clone
+    - Log in to GitHub and locate the[GitHub Repository](https://github.com/gleidecn/quiz-time)
+    - Under the repository name, click "Clone or download".
+    - To clone the repository using HTTPS, under "Clone with HTTPS", copy the link.
+    - Open Git Bash
+    - Change the current working directory to the location where you want the cloned directory to be made.
+    - Type git clone, and then paste the URL you copied in Step 3.
+    $ git clone https://github.com/YOUR-USERNAME/YOUR-REPOSITORY
+    - Press Enter. Your local clone will be created.
+    $ git clone https://github.com/YOUR-USERNAME/YOUR-REPOSITORY
+> Cloning into `CI-Clone`...
+> remote: Counting objects: 10, done.
+> remote: Compressing objects: 100% (8/8), done.
+> remove: Total 10 (delta 1), reused 10 (delta 1)
+> Unpacking objects: 100% (10/10), done.
+
+8. [Credits](#credits)
+
+    - Code Institute for providing endless help and resources to get me this far.
+    - Thanks to Reuben Ferrante, my Code Institute mentor, for his guidance and insight.
+    - Thanks to the Slack community, it was a great resource for issues. 
+    - Color scheme based suggests from [coolors](https://coolors.co/)
+    - W3Schools, for their fhelpful explanations and useful examples.
+    - Youtube video defineletty hwlp me with the Javascript, credit to 
+[Kindson The Tech Pro](https://www.youtube.com/watch?v=2Bpiluefkh8&t=548s)
+
+9. [Media](#media)
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,97 +1,95 @@
 /*  This is a var where I need to make this actions. */
 
-var ul = document.getElementById('ul')
-var nextButton = document.getElementById('btnNext');
-var quizbox = document.getElementById('questionBox')
-var opt1 = document.getElementById('opt1')
-var opt2 = document.getElementById('opt2')
-var opt3 = document.getElementById('opt3')
-var opt4 = document.getElementById('opt4')
+var ul = document.getElementById("ul");
+var nextButton = document.getElementById("btnNext");
+var quizbox = document.getElementById("questionBox");
+var opt1 = document.getElementById("opt1");
+var opt2 = document.getElementById("opt2");
+var opt3 = document.getElementById("opt3");
+var opt4 = document.getElementById("opt4");
 
 /*  This will be the questions where I need to ask the users. */
 
-var app={
-    questions:[
+var app = {
+    questions: [
         {
-            q:'Downing Street is the home of:',
-            options: ['Boris Johnson', 'Harry Potter', 'David Beckham', 'Prince William'],
-            answer:1
+            q: "Downing Street is the home of:",
+            options: ["Boris Johnson", "Harry Potter", "David Beckham", "Prince William"],
+            answer: 1,
         },
         {
-            q:'Witch color represents Distric Line?',
-            options: ['Blue', 'Green', 'Purple', 'Red'],
-            answer:2
-        },      
-        {
-            q:'What was the most popular girls name in the UK in 2019?',
-            options: ['Bruna', 'Greydi', 'Olivia', 'Maria'],
-            answer:3
+            q: "Witch color represents Distric Line?",
+            options: ["Blue", "Green", "Purple", "Red"],
+            answer: 2,
         },
         {
-            q:'Whitch male actor was playing in Titanic?',
-            options: ['Leonardo Da Vinci', 'Jason Statham', 'Brad Pitt', 'Leonardo DiCaprio'],
-            answer:4
-        }
-               
+            q: "Most popular name in the UK in 2019 was?",
+            options: ["Bruna", "Greydi", "Olivia", "Maria"],
+            answer: 3,
+        },
+        {
+            q: "Whitch male actor was playing in Titanic?",
+            options: ["Leonardo Da Vinci", "Jason Statham", "Brad Pitt", "Leonardo DiCaprio"],
+            answer: 4,
+        },
     ],
 
     /*  Depending on the anwsers I´ll get different results. */
 
-    index:0,
-    load:function(){
-        if(this.index<=this.questions.length-1){
-            quizbox.innerHTML=this.index+1 + ". " +this.questions[this.index].q;
-            opt1.innerHTML=this.questions[this.index].options[0];
-            opt2.innerHTML=this.questions[this.index].options[1];
-            opt3.innerHTML=this.questions[this.index].options[2];
-            opt4.innerHTML=this.questions[this.index].options[3];
-        }
-        else {
-            quizbox.innerHTML="Thanks for completing the quiz!";
-            ul.style.display="none";
-            nextButton.style.display="none";
+    index: 0,
+    load: function () {
+        if (this.index <= this.questions.length - 1) {
+            quizbox.innerHTML = this.index + 1 + ". " + this.questions[this.index].q;
+            opt1.innerHTML = this.questions[this.index].options[0];
+            opt2.innerHTML = this.questions[this.index].options[1];
+            opt3.innerHTML = this.questions[this.index].options[2];
+            opt4.innerHTML = this.questions[this.index].options[3];
+        } else {
+            quizbox.innerHTML = "Thanks for completing the quiz!";
+            ul.style.display = "none";
+            nextButton.style.display = "none";
         }
     },
-    next: function(){
+    next: function () {
         this.index++;
         this.load();
     },
-    check: function(ele){
-        var id=ele.id.split('');
-        if(id[id.length-1]==this.questions[this.index].answer){
+    check: function (ele) {
+        var id = ele.id.split("");
+        if (id[id.length - 1] == this.questions[this.index].answer) {
             this.score++;
-            ele.className="correct";
+            ele.className = "correct";
             this.scoreCard();
-        }
-        else{
-            ele.className="wrong";
-        }
-    },
-    preventClick:function(){
-        for(let i=0; i<ul.children.length; i++){
-            ul.children[i].style.pointerEvents="none";
+        } else {
+            ele.className = "wrong";
         }
     },
-    allowClick:function(){
-        for(let i=0; i<ul.children.length; i++){
-            ul.children[i].style.pointerEvents="auto";
-            ul.children[i].className=''
+    preventClick: function () {
+        for (let i = 0; i < ul.children.length; i++) {
+            ul.children[i].style.pointerEvents = "none";
         }
     },
-    score:0,
-    scoreCard:function(){
-        scoreCard.innerHTML=this.questions.length + "/" + this.score;
-    }
+    allowClick: function () {
+        for (let i = 0; i < ul.children.length; i++) {
+            ul.children[i].style.pointerEvents = "auto";
+            ul.children[i].className = "";
+        }
+    },
+    score: 0,
+    scoreCard: function () {
+        scoreCard.innerHTML = this.questions.length + "/" + this.score;
+    },
+};
+
+window.load = app.load();
+
+function button(ele) {
+    app.check(ele);
+    app.preventClick();
 }
 
-window.load=app.load();
-
-function button(ele){
-app.check(ele);
-app.preventClick();
+function next() {
+    app.next();
+    app.allowClick();
 }
 
-function next(){
-app.next();
-app.allowClick();
-}
